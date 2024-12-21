30b1db17d08c969cdb144e455e76d2245f6717b3
Renat Mavlayviev <AlkatrasMaster@yandex.ru>
2024-12-21 04:49:21 +0400

    Метод ThreadPoolExecutor изучено

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,29 +14,40 @@ import java.util.concurrent.*;
 public class Main {
 
     /**
+     * TODO: ThreadPoolExecutor,
+     */
+//    public static void main(String[] args) {
+//        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(5);
+//        executor.getQueue().size();
+//        System.out.println(executor.equals(5));
+//
+//    }
+
+
+    /**
      * TODO: Executors, Executor и ExecutorServic,
      * TODO: Метод Shutdown
      */
 
-    public static void main(String[] args) {
-
-        ExecutorService service = Executors.newSingleThreadExecutor();
-        Future<Double> future = service.submit(() -> {
-            double sum = 0;
-            for (int i = 0; i < 10000; i++) {
-                sum += Math.random();
-            }
-            return sum;
-        });
-        try {
-            System.out.println(future.get());
-            service.shutdown();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } catch (ExecutionException e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    public static void main(String[] args) {
+//
+//        ExecutorService service = Executors.newSingleThreadExecutor();
+//        Future<Double> future = service.submit(() -> {
+//            double sum = 0;
+//            for (int i = 0; i < 10000; i++) {
+//                sum += Math.random();
+//            }
+//            return sum;
+//        });
+//        try {
+//            System.out.println(future.get());
+//            service.shutdown();
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        } catch (ExecutionException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
 
     /**
